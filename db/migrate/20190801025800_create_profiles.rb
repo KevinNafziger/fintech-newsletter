8b2ed7b67f26e8887a03950d80935ccfd50fba56
ROMEODELOSANGELES <ROMEODELOSANGELES@GMAIL.com>
2019-07-31 20:41:12 -0700

    newsletter_app

---
+++ db/migrate/20190801025800_create_profiles.rb
@@ -0,0 +1,12 @@
+class CreateProfiles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :profiles do |t|
+      t.string :email
+      t.string :name
+      t.boolean :user
+      t.boolean :subscriber
+
+      t.timestamps
+    end
+  end
+end
