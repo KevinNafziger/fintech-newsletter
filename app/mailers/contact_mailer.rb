1b77fac18f8d8ce3529ed6c3f8f8f3686aeb2965
romeodelosangeles <41882743+romeodelosangeles@users.noreply.github.com>
2019-08-27 21:33:32 -0700

    fhm logo for mailer
    logo for mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,6 +4,7 @@ class ContactMailer < ApplicationMailer
   # Send a signup email to the user, pass in the user object that contains the user's email address
   def send_signup_email(contact)
     @contact = contact
+    attachments["fhm-logo-transparent_sm.png"] = File.read("#{Rails.root}/public/images/fhm-logo-transparent_sm.png")
       mail( :to => "#{contact.name} <#{contact.email}>",
       :subject => 'Welcome to Fintech Horizons Newsletter' )
     end
