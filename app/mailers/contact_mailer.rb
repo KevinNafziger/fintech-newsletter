c7d2bd1d09fca02a780a90487c02b7638973d51c
unknown <romeodelosangeles@gmail.com>
2019-08-27 22:59:34 -0700

    action mailer test08

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,7 +4,7 @@ class ContactMailer < ApplicationMailer
   # Send a signup email to the user, pass in the user object that contains the user's email address
   def send_signup_email(contact)
     @contact = contact
-    attachments["fhm-logo-transparent_sm.png"] = File.read("#{Rails.root}/public/images/fhm-logo-transparent_sm.png")
+    attachments["banner.png"] = File.read("#{Rails.root}/public/images/banner.png")
       mail( :to => "#{contact.name} <#{contact.email}>",
       :subject => 'Welcome to Fintech Horizons Media Newsletter' )
     end
