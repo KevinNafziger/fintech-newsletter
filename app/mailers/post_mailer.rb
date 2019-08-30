d94c4f3d4e20d0d47c04cf9d7ffb915b5194b88a
unknown <romeodelosangeles@gmail.com>
2019-08-29 19:49:15 -0700

    Post mailer test01

---
+++ app/mailers/post_mailer.rb
@@ -0,0 +1,12 @@
+class PostMailer < ApplicationMailer
+  default :from => 'news@fintechhorizonsmedia.com'
+
+  def new_fhmnews_post(post)
+    @post = post
+    Contact.all.each do |contact|
+      mail( :to => "#{contact.name} <#{contact.email}>", subject: "We have a new interesting post for you...")
+			#or
+      #mail(to: contact.email, subject: "A new interesting post for you ...")
+    end
+  end
+end
