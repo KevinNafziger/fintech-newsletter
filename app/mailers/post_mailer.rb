181e9461e7d45486b21c697891dfda987d077ec4
unknown <romeodelosangeles@gmail.com>
2019-09-01 15:25:32 -0700

    Post mailer test09

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -4,9 +4,9 @@ class PostMailer < ApplicationMailer
   def new_fhmnews_post(post)
     @post = post
 
-    #Contact.all.each do |contact|
-      mail(to: contact.email, subject: "We have a new interesting post for you...")
-    #end
+    Contact.all.each do |contact|
+      mail(to: contact.email, subject: "#{contact.name} We have a new interesting post for you...")
+    end
   end
 end
     # @post = post
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
 class Contact < ApplicationRecord
-  validates :name, :email, presence: true
-  validates :email, uniqueness: true
-
+  validates :name,
+            :email, presence: true
+  validates :email,
+            uniqueness: true,
+            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
 end
