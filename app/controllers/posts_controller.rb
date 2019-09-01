1151374b882000255ca156c64d98fe767c836da5
unknown <romeodelosangeles@gmail.com>
2019-08-31 18:10:59 -0700

    Post mailer test09

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]  
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   # GET /posts
   # GET /posts.json
   def index
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        PostMailer.new_fhmnews_post(@post).deliver
+        PostMailer.new_fhmnews_post(@contact).deliver
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,12 +1,19 @@
 class PostMailer < ApplicationMailer
   default :from => 'news@fintechhorizonsmedia.com'
 
-  def new_fhmnews_post(post)
-    @post = post
+  def new_fhmnews_post(contact)
+    @contact = contact
     Contact.all.each do |contact|
-      #mail( :to => "#{contact.name} <#{contact.email}>", subject: "We have a new interesting post for you...")
-			#or
-      mail(to: contact.email, subject: "We have a new interesting post for you...")
+      mail(:to => "#{contact.name} <#{contact.email}>",
+      subject: "We have a new interesting post for you...")
     end
   end
 end
+    # @post = post
+    # Contact.all.each do |contact|
+    #   #mail( :to => "#{contact.name} <#{contact.email}>", subject: "We have a new interesting post for you...")
+		# 	#or
+    #   mail(to: contact.email, subject: "We have a new interesting post for you...")
+    # end
+#   end
+# end
