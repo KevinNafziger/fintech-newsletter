94a7e2a19ee41ad20ceaa480f27104c5fdb8fab9
unknown <romeodelosangeles@gmail.com>
2019-09-01 14:54:14 -0700

    Post mailer test09

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ApplicationRecord
   validates :name, :email, presence: true
-  validates :email, uniqueness: true  
+  validates :email, uniqueness: true
+
+  def new_fhmnews_post
+    PostMailer.new_fhmnews_post(self).deliver
+
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   has_rich_text :description
   after_create :new_fhmnews_post
 
-  def new_fhmnews_post
-    PostMailer.new_fhmnews_post(self).deliver
-  end
+  # def new_fhmnews_post
+  #   PostMailer.new_fhmnews_post(self).deliver
+  # end
 
 end
