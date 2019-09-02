9c50bfdefdf625fb6a74feaa83de561510722b6d
unknown <romeodelosangeles@gmail.com>
2019-09-01 19:25:41 -0700

    Post mailer test10

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,3 @@
 class Post < ApplicationRecord
-  has_rich_text :description
-  after_create :new_fhmnews_post
-
-  def new_fhmnews_post
-    PostMailer.new_fhmnews_post(self)
-  end
-
+  has_rich_text :description  
 end
