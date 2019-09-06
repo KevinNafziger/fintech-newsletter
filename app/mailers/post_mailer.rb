37d0d4f6fcc6ab4db3b10943bed688a126a98924
unknown <romeodelosangeles@gmail.com>
2019-09-06 15:18:27 -0700

    Post contact publications relationship mailer test12

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -3,9 +3,10 @@ class PostMailer < ApplicationMailer
 
   def new_fhmnews_post(post)
     @post = post
-      Contact.all.each do |contact|
-        mail(to: contact.email, subject: "#{contact.name}, We have a new interesting post for you...")
-      end
+    @contact = Contact.all
+    @contact.each do |contact|
+      mail(to: contact.email, subject: "#{contact.name}, We have a new interesting post for you...")
+    end
   end
 end
     # @post = post
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_040337) do
+ActiveRecord::Schema.define(version: 2019_09_06_040140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,5 +76,16 @@ ActiveRecord::Schema.define(version: 2019_08_23_040337) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "publications", force: :cascade do |t|
+    t.bigint "contact_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contact_id"], name: "index_publications_on_contact_id"
+    t.index ["post_id"], name: "index_publications_on_post_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "publications", "contacts"
+  add_foreign_key "publications", "posts"
 end
