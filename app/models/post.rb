00e36d0b066115b0022f4da2da4444a4dd6daa14
unknown <romeodelosangeles@gmail.com>
2019-09-05 20:24:55 -0700

    Post mailer test12

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ApplicationRecord
   has_rich_text :description
-  # after_save :new_fhmnews_post
+  after_save :new_fhmnews_post
   #
   # private
   #   def new_fhmnews_post
   #     PostMailer.new_fhmnews_post(self).deliver
   #   end
-
+private
   def new_fhmnews_post
     @recipients = Contact.all
     @recipients.each do |r|
