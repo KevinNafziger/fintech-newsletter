a8df560f7be9828b7bce934e63576a05857c523a
unknown <romeodelosangeles@gmail.com>
2019-09-06 18:32:54 -0700

    Post contact publications relationship mailer test12

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,8 +2,7 @@ class PostMailer < ApplicationMailer
   default :from => 'news@fintechhorizonsmedia.com'
 
   def new_fhmnews_post(post, contacts)
-    @post = post
-    @contact = contact
+    @post = post    
     Contact.each.all do |contact|
       mail(to: contact.email, subject: "#{contact.name}, We have a new interesting post for you...")
     end
