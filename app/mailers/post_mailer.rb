2b50be7407582e4ed749ae8aec0a595ed1aa28e6
unknown <romeodelosangeles@gmail.com>
2019-09-07 19:21:29 -0700

    Post contact publications relationship mailer test13

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -5,8 +5,7 @@ class PostMailer < ApplicationMailer
     @post = post
     #Contact.find_each do |contact|
       mail(to: contact.email, subject: "#{contact.name}, We have a new interesting post for you...")
-    end
-  end
+    end  
 end
     # @post = post
     # Contact.all.each do |contact|
