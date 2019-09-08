e1abae943261bd4c8660c60a8b7b741151709cfc
unknown <romeodelosangeles@gmail.com>
2019-09-07 20:14:55 -0700

    Post contact publications relationship mailer test13

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        Publication.Contact.find_each do |contact|
+        Contact.all.each do |contact|
           PostMailer.new_fhmnews_post(@post).deliver
         end
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
