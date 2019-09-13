139034694f1ff35dcd73e8331b00932bae4c7ce0
unknown <romeodelosangeles@gmail.com>
2019-09-12 20:53:54 -0700

    Active Job for Post emails with delayed_job test01

--- app/jobs/post_email_job.rb
+++ app/jobs/post_email_job.rb
@@ -1,9 +1,10 @@
 class PostEmailJob < ApplicationJob
   queue_as :default
 
-  def perform(post)
+  def perform(post, contact)
     # Do something later
     @post = post
+    @contact = contact
     PostMailer.new_fhmnews_post(@post, @contact).deliver_later
   end
 end
