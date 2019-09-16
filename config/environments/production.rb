424cee438bca73f35063fcff87f850cde31725ec
unknown <romeodelosangeles@gmail.com>
2019-09-15 17:24:12 -0700

    Post Mailer with delayed_job test02

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.active_record.queue_adapter = :delayed_job
+  config.active_job.queue_adapter = :delayed_job
 end
