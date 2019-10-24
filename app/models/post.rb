bc1f545994efa7411461af1c973bb810cbe68898
unknown <romeodelosangeles@gmail.com>
2019-10-23 22:53:15 -0700

    Adding fhmnews facebook login

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_rich_text :description
-
+  belongs_to :user
   has_many :publications
   has_many :contacts, through: :publications
   #after_save :new_fhmnews_post
