1cc6248787bedc433a191995522e57211a23ab1c
unknown <romeodelosangeles@gmail.com>
2019-10-24 18:43:43 -0700

    Adding fhmnews facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :contacts
   root to: 'home#index'
   resources :articles
-  resources :posts    
+  resources :posts
   resources :profiles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
