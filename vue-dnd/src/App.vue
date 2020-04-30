3f0578edc73a4b8ce3a19e5e4e84a92960768bd2
celfonica <servicioscelfonica@gmail.com>
2020-04-30 15:11:24 -0700

    Card.vue component | update

--- vue-dnd/src/App.vue
+++ vue-dnd/src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
   <main class="flexbox">
-<center>  Hello, please let me know if you see this, Thanks.
-<br><br>
-  Hola, avisame si miras esto porfavor, Gracias.
-  </center>
+
+    <Board id="board-1">
+     <Card id="card-1" draggable="true">
+       <p> Card one </p>
+      </Card>
+    </Board>
+
+    <Board id="board-2">
+     <Card id="card-2" draggable="true">
+       <p> Card two </p>
+      </Card>
+    </Board>
+
   </main>
 
   </div>
 </template>
 
 <script>
+import Board from './components/Board';
+import Card from './components/Card';
+
 export default {
-  name: 'App',
+  name: 'app',
   components: {
+  Board,
+   Card
   }
 }
 </script>
