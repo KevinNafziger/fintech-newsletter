bcf064603f2ca445ff044371ebf3f350fd19411c
celfonica <servicioscelfonica@gmail.com>
2020-05-13 15:08:48 -0700

    Vue js chart component on builder view

--- app/javascript/packs/charts.js
+++ app/javascript/packs/charts.js
@@ -1,3 +1,13 @@
 
 
 import PureVueChart from 'pure-vue-chart';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    render: h => h(App)
+  }).$mount()
+  document.body.appendChild(app.$el)
+
+  console.log(app)
+})
